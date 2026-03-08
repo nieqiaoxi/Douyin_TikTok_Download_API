c226c61d434ed54f4a8735fc78fa97798c4ff8bb
qianlu <2556844982@qq.com>
2026-03-08 13:15:14 +0800

    fix: bug

--- crawlers/base_crawler.py
+++ crawlers/base_crawler.py
@@ -61,9 +61,9 @@ class BaseCrawler:
     def __init__(
             self,
             proxies: dict = None,
-            max_retries: int = 3,
+            max_retries: int = 1,
             max_connections: int = 50,
-            timeout: int = 10,
+            timeout: int = 5,
             max_tasks: int = 50,
             crawler_headers: dict = {},
     ):
